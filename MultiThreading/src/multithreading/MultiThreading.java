4845291f2a7efe876031653bead81650812a4855
xyclose11 <harrisgus1@hotmail.com>
2023-12-15 10:53:11 -0600

    Modified multithreading

--- MultiThreading/src/multithreading/MultiThreading.java
+++ MultiThreading/src/multithreading/MultiThreading.java
@@ -1,4 +1,17 @@
 package multithreading;
 
-public class MultiThreading {
+public class MultiThreading extends Thread{
+    String message;
+    MultiThreading ( String message ) { this.message = message; };
+    public void run() {
+        System.out.println( message );
+    }
+    public static void main(String[] args) {
+        MultiThreading ht1 = new MultiThreading("Thread 1");
+        MultiThreading ht2 = new MultiThreading("Thread 2");
+        MultiThreading ht3 = new MultiThreading("Thread 3");
+        ht1.start();
+        ht2.start();
+        ht3.start();
+    }
 }
