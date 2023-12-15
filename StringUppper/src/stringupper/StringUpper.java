8826beeb1e5234be3cc9c1eae597762b83bc98c9
xyclose11 <harrisgus1@hotmail.com>
2023-12-15 10:25:33 -0600

    Added a StringBuffer example, which is mutuble

--- StringUppper/src/stringupper/StringUpper.java
+++ StringUppper/src/stringupper/StringUpper.java
@@ -4,5 +4,9 @@ public class StringUpper {
     public static void main(String[] args) {
         String exampleSentence = "There once was a man who loved to fish, then one day there were no more fish.";
         System.out.println(exampleSentence.toUpperCase());
+
+        StringBuffer sB = new StringBuffer("Hello");
+        sB.append(" World!");
+        System.out.println(sB);
     }
 }
