f2205472be0b338a4bcecba7ba9a557ec19bc33d
xyclose11 <harrisgus1@hotmail.com>
2023-12-15 09:58:44 -0600

    Fully implemented 'Basics' program with command line arguments

--- Basics/src/basics/Basics.java
+++ Basics/src/basics/Basics.java
@@ -1,4 +1,14 @@
 package basics;
 
 public class Basics {
+    public static void main(String args[]) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        z = x + y;
+        System.out.println("x + y = " + z);
+    }
 }
