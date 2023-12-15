56a4664b6676f6780dfc55f284874d2fec6648c7
xyclose11 <harrisgus1@hotmail.com>
2023-12-15 10:22:45 -0600

    Finished Example 3.3 interpretation

--- StringUppper/src/stringupper/StringUpper.java
+++ StringUppper/src/stringupper/StringUpper.java
@@ -1,4 +1,8 @@
 package stringupper;
 
 public class StringUpper {
+    public static void main(String[] args) {
+        String exampleSentence = "There once was a man who loved to fish, then one day there were no more fish.";
+        System.out.println(exampleSentence.toUpperCase());
+    }
 }
